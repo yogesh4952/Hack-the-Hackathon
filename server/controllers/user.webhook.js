0fda7c93d994b4845ce122609bd75e18e401a5bb
yogesh4952 <yogeshshah2063@gmail.com>
2025-01-30 22:34:31 +0545

    Add .js ext

--- server/controllers/user.webhook.js
+++ server/controllers/user.webhook.js
@@ -1,5 +1,5 @@
 import { Webhook } from 'svix';
-import User from '../models/user.models';
+import User from '../models/user.models.js';
 
 export const webhookController = async (req, res) => {
   try {
